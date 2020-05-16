dd0383f90e6a9d2320c4aa9a59a15b6673772a46
miroslawmajka <go2mirek@gmail.com>
2020-05-16 22:00:00 +0100

    More boilerplate stuff

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,9 +8,44 @@ terraform {
   backend "s3" {}
 }
 
+###############################
+# STEP 1 - static website in S3
+###############################
+
 # The AWS S3 static website bucket with workspace/environment name appended
 resource "aws_s3_bucket" "website-bucket" {
   bucket = "aws-serverless-app-${terraform.workspace}"
 }
 
+# TODO: use terraform random provider to randomize the bucket name as they need to be globally unique
+
 # TODO: make the bucket a "static website" that can serve content publicly
+
+# TODO: return URL from terraform so it can be accessed by automated UI tests in a step down the line
+
+
+###########################
+# STEP 2 - Lambda functions
+###########################
+
+# TODO: create the Node function(s)
+
+# TODO: create the Python function(s)
+
+
+######################
+# STEP 3 - API Gateway
+######################
+
+# TODO: crete endpoints that point to Lambda functions
+
+# TODO: CORS for the website to use the gateway functions
+
+# TODO: figure out authorization
+
+
+#########################
+# STEP 4 - Cognito
+#########################
+
+# TODO: create user pool and save details for one user
