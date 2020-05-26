c34a4ff71bdc5948d780796d5d1b4f54435e325a
miroslawmajka <go2mirek@gmail.com>
2020-05-26 21:43:21 +0100

    Adding clever Terraform workspace select logic
    Added gitattributes for Windows LF file ending in shell scripts et al.

---
+++ .eslintrc.js
@@ -0,0 +1,97 @@
+module.exports = {
+    env: {
+        node: true,
+        es6: true,
+        mocha: true,
+        browser: true
+    },
+    extends: 'eslint:recommended',
+    parserOptions: {
+        sourceType: 'module',
+        ecmaVersion: 2017
+    },
+    rules: {
+        'no-undef': 'off',
+        'no-var': 'error',
+        'newline-before-return': 'error',
+        'prefer-arrow-callback': 'warn',
+        'no-console': 'off',
+        'prefer-template': 'error',
+        'no-unused-vars': [
+            'error',
+            {
+                'args': 'none'
+            }
+        ],
+        indent: [
+            'error',
+            4,
+            {
+                'SwitchCase': 1
+            }
+        ],
+        quotes: [
+            'error',
+            'single'
+        ],
+        'quote-props': [
+            'error',
+            'as-needed'
+        ],
+        semi: [
+            'error',
+            'always'
+        ],
+        strict: [
+            'error',
+            'global'
+        ],
+        'arrow-parens': [
+            'error',
+            'as-needed'
+        ],
+        'comma-dangle': [
+            'error',
+            'never'
+        ],
+        'space-before-function-paren': [
+            'error',
+            'never'
+        ],
+        'object-curly-spacing': [
+            'error',
+            'always'
+        ],
+        'space-in-parens': [
+            'error',
+            'never'
+        ],
+        'array-bracket-spacing': [
+            'error',
+            'never'
+        ],
+        'no-multiple-empty-lines': [
+            'error',
+            {
+                'max': 1,
+                'maxEOF': 0,
+                'maxBOF': 0
+            }
+        ],
+        'comma-spacing': [
+            'error',
+            {
+                'before': false,
+                'after': true
+            }
+        ],
+        'space-before-blocks': [
+            'error',
+            {
+                'functions': 'always',
+                'keywords': 'always',
+                'classes': 'always'
+            }
+        ]
+    }
+};
---
+++ terraform/select-workspace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+echo "Determining if $1 workspace exists..."
+
+if [ $(terraform workspace list | grep -c "$1") -eq 0 ] ; then
+  echo "Creating new $1 workspace..."
+  terraform workspace new "$1" -no-color
+else
+  echo "Selecting existing $1 workspace..."
+  terraform workspace select "$1" -no-color
+fi
