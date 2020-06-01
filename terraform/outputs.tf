14657ca9edd5afecd02deb26a9fecb90abc95ad2
miroslawmajka <go2mirek@gmail.com>
2020-06-01 21:05:58 +0100

    Adding website deployment - stage 1

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,14 @@
 output "website_url" {
-  value = "${aws_s3_bucket.website_bucket.website_endpoint}"
+  value = "http://${aws_s3_bucket.website_bucket.website_endpoint}"
   description = "The AWS Serverless App website URL"
 }
 
 output "serverless_rest_api_base_url" {
   value = "${aws_api_gateway_deployment.dynamic_deployment.invoke_url}"
   description = "The AWS Serverless App REST API URL"
+}
+
+output "website_bucket_name" {
+  value = "${aws_s3_bucket.website_bucket.bucket}"
+  description = "The AWS Serverless App bucket name"
 }
