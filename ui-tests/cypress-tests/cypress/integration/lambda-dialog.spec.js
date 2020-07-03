8e9c319affd4e0961e7e6ed02dc0db389dc84d59
miroslawmajka <go2mirek@gmail.com>
2020-07-03 20:28:08 +0100

    Defensive bash scripting in pipelines
    Refactored Cypress selectors
    Added lint task to the vscode tasks

--- ui-tests/cypress-tests/cypress/integration/lambda-dialog.spec.js
+++ ui-tests/cypress-tests/cypress/integration/lambda-dialog.spec.js
@@ -1,22 +1,26 @@
 /// <reference types="cypress" />
 
 context('Lambda Dialog', () => {
-    const NODE_HELLO = '#btnTestNodeHelloLambda';
-    const PYTHON_HELLO = '#btnTestPythonHelloLambda';
-    const NODE_LOTTERY = '#btnTestNodeLotteryLambda';
+    const SELECTOR_NODE_HELLO = '#btnTestNodeHelloLambda';
+    const SELECTOR_PYTHON_HELLO = '#btnTestPythonHelloLambda';
+    const SELECTOR_NODE_LOTTERY = '#btnTestNodeLotteryLambda';
+    const SELECTOR_OUTPUT = '#txtLambdaOuput';
+    const SELECTOR_DIALOG = '#dlgLambdaDialog';
+    const SELECTOR_OPEN = '#btnLambdaDialogOpen';
+    const SELECTOR_CLOSE = '#btnDialogClose';
 
     const dialogButtons = [
         {
             name: 'Node Hello',
-            selector: NODE_HELLO
+            selector: SELECTOR_NODE_HELLO
         },
         {
             name: 'Python Hello',
-            selector: PYTHON_HELLO
+            selector: SELECTOR_PYTHON_HELLO
         },
         {
             name: 'Node Lottery',
-            selector: NODE_LOTTERY
+            selector: SELECTOR_NODE_LOTTERY
         }
     ];
 
@@ -25,53 +29,49 @@ context('Lambda Dialog', () => {
     });
 
     it('is opened upon clicking the button and specific button trigger Lambda calls', () => {
-        cy.get('#btnLambdaDialogOpen').click();
+        cy.get(SELECTOR_OPEN).click();
 
-        cy.get('#dlgLambdaDialog').should('be.visible');
+        cy.get(SELECTOR_DIALOG).should('be.visible');
+        cy.get(SELECTOR_OUTPUT).should('have.value', '');
 
-        cy.get('#txtLambdaOuput').should('have.value', '');
+        cy.get(SELECTOR_NODE_HELLO).click();
 
-        cy.get(NODE_HELLO).click();
+        cy.get(SELECTOR_OUTPUT).should('have.value', 'Hello from Lambda Node!');
 
-        cy.get('#txtLambdaOuput').should('have.value', 'Hello from Lambda Node!');
+        cy.get(SELECTOR_PYTHON_HELLO).click();
 
-        cy.get(PYTHON_HELLO).click();
+        cy.get(SELECTOR_OUTPUT).should('have.value', 'Hello from Lambda Python!');
 
-        cy.get('#txtLambdaOuput').should('have.value', 'Hello from Lambda Python!');
+        cy.get(SELECTOR_NODE_LOTTERY).click();
 
-        cy.get(NODE_LOTTERY).click();
-
-        cy.get('#txtLambdaOuput').should('not.have.value', 'Hello from Lambda Node!');
-        cy.get('#txtLambdaOuput').should('not.have.value', 'Hello from Lambda Python!');
+        cy.get(SELECTOR_OUTPUT).should('not.have.value', 'Hello from Lambda Node!');
+        cy.get(SELECTOR_OUTPUT).should('not.have.value', 'Hello from Lambda Python!');
 
         cy.get('#btnClearLambdaOutput').click();
 
-        cy.get('#txtLambdaOuput').should('have.value', '');
+        cy.get(SELECTOR_OUTPUT).should('have.value', '');
 
-        cy.get('#btnDialogClose').click();
+        cy.get(SELECTOR_CLOSE).click();
 
-        cy.get('#dlgLambdaDialog').should('not.be.visible');
+        cy.get(SELECTOR_DIALOG).should('not.be.visible');
     });
 
     dialogButtons.forEach(b => {
         it(`is opened with a clean output box after using the ${b.name} button`, () => {
-            cy.get('#btnLambdaDialogOpen').click();
+            cy.get(SELECTOR_OPEN).click();
 
-            cy.get('#dlgLambdaDialog').should('be.visible');
-
-            cy.get('#txtLambdaOuput').should('have.value', '');
+            cy.get(SELECTOR_DIALOG).should('be.visible');
+            cy.get(SELECTOR_OUTPUT).should('have.value', '');
 
             cy.get(b.selector).click();
+            cy.get(SELECTOR_CLOSE).click();
 
-            cy.get('#btnDialogClose').click();
+            cy.get(SELECTOR_DIALOG).should('not.be.visible');
 
-            cy.get('#dlgLambdaDialog').should('not.be.visible');
+            cy.get(SELECTOR_OPEN).click();
 
-            cy.get('#btnLambdaDialogOpen').click();
-
-            cy.get('#dlgLambdaDialog').should('be.visible');
-
-            cy.get('#txtLambdaOuput').should('have.value', '');
+            cy.get(SELECTOR_DIALOG).should('be.visible');
+            cy.get(SELECTOR_OUTPUT).should('have.value', '');
         });
     });
 });
